59b42b3c7a05cb3db267e0b52a790e078628926e
yadvi1804 <yadvi.1804@gmail.com>
2026-08-24 21:03:48 +0530

    Remove insecure SSH rule

--- main.tf
+++ main.tf
@@ -1,15 +1,3 @@
 terraform {
   required_version = ">= 1.0"
 }
-
-resource "aws_security_group" "insecure_ssh" {
-  name = "insecure-ssh"
-
-  ingress {
-    description = "SSH from anywhere"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
